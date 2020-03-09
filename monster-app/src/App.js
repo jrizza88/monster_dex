fc741d57e33e71dca3d525a9aaed4e21cc483c13
Jamar Torres <Jetorres88@gmail.com>
2020-03-09 19:19:58 -0400

    fetched through a promise the api data needed and returned the data as an array

--- monster-app/src/App.js
+++ monster-app/src/App.js
@@ -6,22 +6,32 @@ class App extends React.Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        }
-      ]
+      monsters: []
+      //   {
+      //     name: 'Frankenstein',
+      //     id: 'asc1'
+      //   },
+      //   {
+      //     name: 'Dracula',
+      //     id: 'asc2'
+      //   },
+      //   {
+      //     name: 'Zombie',
+      //     id: 'asc3'
+      //   }
+      // ]
     };
   }
+
+  componentDidMount() {
+    // this calls whatever block of code being run
+    fetch('https://jsonplaceholder.typicode.com/users')
+    // returns a promise (then)
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+    // .then(promise => console.log(promise))
+  }
+
   render(){
     return (
       <div className="App">
