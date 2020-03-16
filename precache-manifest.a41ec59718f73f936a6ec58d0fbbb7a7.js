752e4db25cc944133d8ecde0bd747c15b65bf403
Jamar Torres <Jetorres88@gmail.com>
2020-03-16 19:21:57 -0400

    Updates

---
+++ precache-manifest.a41ec59718f73f936a6ec58d0fbbb7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea611dec007cb0e41f5fa4ed35e2ac92",
+    "url": "/monster_dex/index.html"
+  },
+  {
+    "revision": "8d231696eec674d529c3",
+    "url": "/monster_dex/static/css/main.4cd8e30b.chunk.css"
+  },
+  {
+    "revision": "2eeba163add5b87c789c",
+    "url": "/monster_dex/static/js/2.580e123d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster_dex/static/js/2.580e123d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d231696eec674d529c3",
+    "url": "/monster_dex/static/js/main.9a78e813.chunk.js"
+  },
+  {
+    "revision": "5ab336650137ec3af44f",
+    "url": "/monster_dex/static/js/runtime-main.b55dcad3.js"
+  }
+]);
